ac870fbc5873ceb405e5db77572bd9c02d0b14d8
Mohamad Chamra <mohamad.chamra@navybits.com>
2022-06-09 00:53:02 +0300

    update

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ fetch('http://my-restaurant-101.herokuapp.com/data')
       .then(response => response.json())
       .then(json => {          
             console.log(json)
+            
           });
